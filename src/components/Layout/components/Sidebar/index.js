7c113ce6cd165fa8ebc3a3f2a3be827dd9470a82
trinh.tx <trinh.tx@teko.vn>
2022-11-29 12:26:20 +0700

    multi lang for sidebar

--- src/components/Layout/components/Sidebar/index.js
+++ src/components/Layout/components/Sidebar/index.js
@@ -5,6 +5,8 @@ import { CalculatorOutlined, DatabaseOutlined } from '@ant-design/icons';
 import classNames from 'classnames/bind';
 import styles from './Sidebar.module.scss';
 
+import { t } from '~/helpers/i18n';
+
 const cx = classNames.bind(styles);
 
 function getItem(label, key, icon, children, type) {
@@ -18,18 +20,18 @@ function getItem(label, key, icon, children, type) {
 }
 
 const items = [
-  getItem('Toán', 'sub1', <CalculatorOutlined />, [
-    getItem('Đại số', '2'),
+  getItem(t('Category.Math'), 'sub1', <CalculatorOutlined />, [
+    getItem(t('Category.Algebra'), '2'),
     // getItem('Giải tích', 'sub2', null, [getItem('Giải tích 1', '3'), getItem('Giải tích 2', '3')]),
-    getItem('Giải tích', '3'),
-    getItem('Toán rời rạc', '4')
+    getItem(t('Category.Calculus'), '3'),
+    getItem(t('Category.DiscreteMathematics'), '4')
   ]),
-  getItem('Cấu trúc dữ liệu & Giải thuật', '5'),
-  getItem('Dữ liệu', 'sub3', <DatabaseOutlined />, [
-    getItem('Cơ sơ dữ liệu', '6'),
-    getItem('Kho dữ liệu', '7')
+  getItem(t('Category.DataStructuresAlgorithms'), '5'),
+  getItem(t('Category.Data'), 'sub3', <DatabaseOutlined />, [
+    getItem(t('Category.Database'), '6'),
+    getItem(t('Category.DataWarehouse'), '7')
   ]),
-  getItem('Khác', '12'),
+  getItem(t('Category.Other'), '12'),
 ]; // submenu keys of first level
 
 const rootSubmenuKeys = ['sub1', 'sub3'];
